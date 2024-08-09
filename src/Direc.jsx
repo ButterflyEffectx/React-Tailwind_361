89e79bc4dac29b738be77024a1cbcc0db63543cc
ButterflyEffectx <onyoux755@gmail.com>
2024-08-09 18:33:47 +0700

    create IndexPage/Directory

---
+++ src/Direc.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css'
+import Nav from './components/nav'
+import Footer from './components/footer'
+import HeaderDirec from './components/headerDirec'
+
+function Directory() {
+
+  return (
+    <>
+      <Nav />
+      <HeaderDirec />
+      
+      <Footer />
+    </>
+  )
+}
+
+export default Directory
---
+++ src/components/headerDirec.jsx
@@ -0,0 +1,188 @@
+import React from 'react'
+
+function HeaderDirec() {
+  return (
+    <div class="flex justify-center">
+    
+
+<div class="w-full p-4 bg-white border border-gray-200  shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex items-center justify-between mb-4">
+        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">วิทยาการคอมพิวเตอร์ รุ่นที่ 29</h5>
+        <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+            View all
+        </a>
+   </div>
+   <div class="flow-root">
+        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+            <li class="py-3 sm:py-4">
+                <div class="flex items-center">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Neil image"></img>
+                    </div>
+                    <div class="flex-1 min-w-0 ms-4">
+                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            6604101361
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            พีระพัฒน์ มิคะนุช
+                        </p>
+                    </div>
+                    <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                        <a href="#">ดูโปรไฟล์</a>
+                    </div>
+                </div>
+            </li>
+            <li class="py-3 sm:py-4">
+                <div class="flex items-center ">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"></img>
+                    </div>
+                    <div class="flex-1 min-w-0 ms-4">
+                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            6604101361
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            พีระพัฒน์ มิคะนุช
+                        </p>
+                    </div>
+                    <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                        <a href="#">ดูโปรไฟล์</a>
+                    </div>
+                </div>
+            </li>
+            <li class="py-3 sm:py-4">
+                <div class="flex items-center">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-2.jpg" alt="Michael image"></img>
+                    </div>
+                    <div class="flex-1 min-w-0 ms-4">
+                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            6604101361
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            พีระพัฒน์ มิคะนุช
+                        </p>
+                    </div>
+                    <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                        <a href="#">ดูโปรไฟล์</a>
+                    </div>
+                </div>
+            </li>
+            <li class="py-3 sm:py-4">
+                <div class="flex items-center ">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-4.jpg" alt="Lana image"></img>
+                    </div>
+                    <div class="flex-1 min-w-0 ms-4">
+                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            6604101361
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            พีระพัฒน์ มิคะนุช
+                        </p>
+                    </div>
+                    <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                        <a href="#">ดูโปรไฟล์</a>
+                    </div>
+                </div>
+            </li>
+            <li class="pt-3 pb-0 sm:pt-4">
+                <div class="flex items-center ">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Thomas image"></img>
+                    </div>
+                    <div class="flex-1 min-w-0 ms-4">
+                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            6604101361
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            พีระพัฒน์ มิคะนุช
+                        </p>
+                    </div>
+                    <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                        <a href="#">ดูโปรไฟล์</a>
+                    </div>
+                </div>
+            </li>
+            <li class="py-3 sm:py-4">
+                <div class="flex items-center ">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"></img>
+                    </div>
+                    <div class="flex-1 min-w-0 ms-4">
+                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            6604101361
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            พีระพัฒน์ มิคะนุช
+                        </p>
+                    </div>
+                    <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                        <a href="#">ดูโปรไฟล์</a>
+                    </div>
+                </div>
+            </li>
+            <li class="py-3 sm:py-4">
+                <div class="flex items-center ">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"></img>
+                    </div>
+                    <div class="flex-1 min-w-0 ms-4">
+                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            6604101361
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            พีระพัฒน์ มิคะนุช
+                        </p>
+                    </div>
+                    <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                        <a href="#">ดูโปรไฟล์</a>
+                    </div>
+                </div>
+            </li>
+            <li class="py-3 sm:py-4">
+                <div class="flex items-center ">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"></img>
+                    </div>
+                    <div class="flex-1 min-w-0 ms-4">
+                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            6604101361
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            พีระพัฒน์ มิคะนุช
+                        </p>
+                    </div>
+                    <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                        <a href="#">ดูโปรไฟล์</a>
+                    </div>
+                </div>
+            </li>
+            <li class="py-3 sm:py-4">
+                <div class="flex items-center ">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"></img>
+                    </div>
+                    <div class="flex-1 min-w-0 ms-4">
+                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            6604101361
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            พีระพัฒน์ มิคะนุช
+                        </p>
+                    </div>
+                    <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                        <a href="#">ดูโปรไฟล์</a>
+                    </div>
+                </div>
+            </li>
+        </ul>
+   </div>
+</div>
+
+
+</div>
+  )
+}
+
+export default HeaderDirec
